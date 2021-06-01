fa3fe01f4d1562229b6287853528708d54d25a77
hirenkarangiya <hiren.karangiya92@gmail.com>
2021-06-01 10:09:42 +0530

    create new components and style menu items

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 
-import HomePage from './HomePage.component';
+import HomePage from './pages/homepage/HomePage.component';
 
 function App() {
   return (
---
+++ src/pages/homepage/HomePage.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './HomePage.style.scss';
+
+import Directory from '../../components/directory/directory.component'
+
+function HomePage() {
+    return (
+        <div className="homepage">
+            <Directory />
+        </div>
+    )
+}
+
+export default HomePage
